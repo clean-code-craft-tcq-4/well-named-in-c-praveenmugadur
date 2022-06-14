65fa7a6c80453f99b7b8cf870da398b40d429db4
praveenmugadur <praveenmugadur5@gmail.com>
2022-06-14 12:55:22 +0530

    Update testfunctions.h

--- testfunctions.h
+++ testfunctions.h
@@ -2,7 +2,7 @@
 #define TESTCOLORPAIR_H
 
 #include <assert.h>
-#include "ColorPair.h"
+#include "colorcode.h"
 
 extern void testNumberToPair(int pairNumber,    MajorColor_type expectedMajor,     MinorColor_type expectedMinor);
 extern void testPairToNumber(     MajorColor_type major,     MinorColor_type minor,     int expectedPairNumber);
